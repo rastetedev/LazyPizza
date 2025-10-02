0c051b15100b3155a191b9e1ca0e49cf34b38eca
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2025-10-02 13:02:55 -0500

    Refactor LPPrimaryButton to show shadow

--- app/src/main/java/com/raulastete/lazypizza/ui/components/LPPrimaryButton.kt
+++ app/src/main/java/com/raulastete/lazypizza/ui/components/LPPrimaryButton.kt
@@ -2,16 +2,23 @@ package com.raulastete.lazypizza.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.raulastete.lazypizza.ui.theme.AppTheme
+import com.raulastete.lazypizza.ui.theme.Orange
 
 @Composable
 fun LPPrimaryButton(
@@ -21,9 +28,18 @@ fun LPPrimaryButton(
 ) {
     Box(
         modifier = modifier
-            .clickable {
-                onClick()
+            .graphicsLayer {
+                shadowElevation = 16f
+                spotShadowColor = Orange
+                shape = RoundedCornerShape(100.dp)
+                clip = true
             }
+            .clip(AppTheme.shape.button)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = ripple(),
+                onClick = onClick
+            )
             .background(
                 brush = AppTheme.colorScheme.primaryGradient,
                 shape = AppTheme.shape.button
@@ -31,7 +47,8 @@ fun LPPrimaryButton(
             .padding(
                 vertical = 10.dp,
                 horizontal = 24.dp
-            ), contentAlignment = Alignment.Center
+            ),
+        contentAlignment = Alignment.Center
     ) {
         Text(
             text = text,
@@ -48,7 +65,7 @@ private fun LPPrimaryButtonPreview() {
         LPPrimaryButton(
             modifier = Modifier.fillMaxWidth(),
             text = "Add to Cart for $12.99"
-        ){
+        ) {
 
         }
     }
